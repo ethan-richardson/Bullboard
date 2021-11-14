4e56b71127b69f20a2c2e4e1aa8fb0ed8ce1621c
Zayaan <rahmanzayaan43@gmail.com>
2021-11-14 16:23:23 -0500

    WebSockets working - but w/ a diff. library

--- backend/actions.py
+++ backend/actions.py
@@ -1,31 +1,27 @@
 import respond, file
 # We want to serve the login page here.
-def login(server):
-    file_ = file.read_file("Bullboard/frontend/pages/login.html")
-    respond.send_response(server, b"200", b"text/html", str(len(file_)).encode())
-    server.wfile.write(file_)
+def login():
+    body = file.read_file("Bullboard/frontend/pages/login.html")
+    response_code = 200
+    content_type = "text/html"
+    return [body, response_code, content_type]
 
 # We want to serve the register page here.
-def register(server):
-    html = b"Register page here."
-    respond.send_response(server, b"200", b"text/html", str(len(html)).encode())
-    server.wfile.write(html)
+def register():
+    body = b"Register page here."
+    response_code = 200
+    content_type = "text/html"
+    return [body, response_code, content_type]
 
 # Respond to HTML paths here.
-def resp_to_html_paths(server):
-    file_= file.read_file("Bullboard/frontend/pages%s" % server.path)
-    if server.path.endswith(".css"):
-        respond.send_response(server, b"200", b"text/css", str(len(file_)).encode())
-    elif server.path.endswith(".js"):
-        respond.send_response(server, b"200", b"text/javascript", str(len(file_)).encode())
-    elif server.path.endswith(".png"):
-        respond.send_response(server, b"200", b"image/png", str(len(file_)).encode())
+def resp_to_html_paths(path):
+    body = file.read_file("Bullboard/frontend/pages%s" % path)
+    response_code = 200
+    if path.endswith(".css"):
+        return [body, response_code, "text/css"]
+    elif path.endswith(".js"):
+        return [body, response_code, "text/javascript"]
+    elif path.endswith(".png"):
+        return [body, response_code, "image/png"]
     else:
-        respond.send_response(server, b"200", b"image/jpeg", str(len(file_)).encode())
-    server.wfile.write(file_)
-
-def handle_socket(server):
-    print("hello")
-    pass
-    #respond.send_response(server, b"101", b"")
-# TODO - Other functions dependent on the path will also go here #
+        return [body, response_code, "image/jpeg"]
