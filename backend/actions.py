9507ad2929604fc6a80b88c2689455148b250bd0
Cindy C <cjcheng@buffalo.edu>
2021-11-24 23:47:32 -0500

    updated

---
+++ backend/actions.py
@@ -0,0 +1,27 @@
+import file
+
+def login():
+    body = file.read_file("Bullboard/frontend/pages/login.html")
+    response_code = 200
+    content_type = "text/html"
+    return [body, response_code, content_type]
+
+# We want to serve the register page here.
+def register():
+    body = b"Register page here."
+    response_code = 200
+    content_type = "text/html"
+    return [body, response_code, content_type]
+
+# Respond to HTML paths here.
+def resp_to_html_paths(path):
+    body = file.read_file("Bullboard/frontend/pages%s" % path)
+    response_code = 200
+    if path.endswith(".css"):
+        return [body, response_code, "text/css"]
+    elif path.endswith(".js"):
+        return [body, response_code, "text/javascript"]
+    elif path.endswith(".png"):
+        return [body, response_code, "image/png"]
+    else:
+        return [body, response_code, "image/jpeg"]
---
+++ backend/routes.py
@@ -0,0 +1,18 @@
+import actions
+
+# Dictionary holding all possible GET requests
+get_routes = {
+    "/login":actions.login,
+    "/": actions.login,
+    "/register": actions.register,
+    "/functions.js":actions.resp_to_html_paths,
+    "/styles.css":actions.resp_to_html_paths,
+    "/Bull_Board_Mat.png":actions.resp_to_html_paths,
+    "/bull_knocker.jpeg": actions.resp_to_html_paths,
+    "/welcome_mat.png": actions.resp_to_html_paths,
+}
+
+# Dictionary holding all possible POST requests
+post_routes = {
+    "/create_account.html":actions.create_account
+}
